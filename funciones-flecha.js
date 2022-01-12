496e196a80ca327878b80863fa0d1925e0748a9e
LucasRivero01 <lucas.rivero@naranjax.com>
2022-01-12 18:39:14 -0300

    segundo commit

---
+++ funciones-flecha.js
@@ -0,0 +1,15 @@
+var sumar = (a, b) => {
+    return a + b;
+}
+
+var restar = (a, b) => {
+    return a - b;
+}
+
+var dividir = (a, b) => {
+    return a / b;
+}
+
+var multiplicar = (a, b) => {
+    return a * b;
+}
